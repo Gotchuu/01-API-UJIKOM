90f5f9655f5258f36e19bdb18ad4e735bc628cd9
Kevin Naza Reyfansyah <knrexe123@gmail.com>
2026-09-08 19:55:15 +0700

    add (Index Pesan Admin) : Penambahan pada model PesanPerbaikan

--- backend/app/Models/PesanPerbaikan.php
+++ backend/app/Models/PesanPerbaikan.php
@@ -6,7 +6,19 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PesanPerbaikan extends Model {
     protected $table = 'pesan_perbaikan';
-    protected $fillable = ['pengembalian_id','petugas_id','jenis','pesan','status'];
-    public function pengembalian(): BelongsTo { return $this->belongsTo(Pengembalian::class); }
-    public function petugas(): BelongsTo { return $this->belongsTo(User::class, 'petugas_id'); }
+    protected $fillable = [
+        'pengembalian_id','petugas_id','jenis','pesan','status','admin_id','admin_catatan'
+        ];
+    public function admin(): BelongsTo 
+    { return $this->belongsTo(User::class, 'admin_id'); 
+        }
+
+    public function pengembalian(): BelongsTo { 
+        return $this->belongsTo(Pengembalian::class); 
+        }
+
+    public function petugas(): BelongsTo { 
+        return $this->belongsTo(User::class, 'petugas_id'); 
+        }
+        
 }
