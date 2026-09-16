154a470d8a9c3687d1f3c0e4e3f12d91e0dba08b
Kevin Naza Reyfansyah <knrexe123@gmail.com>
2026-09-16 09:02:08 +0700

    Fix (Layout Petugas) : Fix navbar petugas duplikat

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -54,15 +54,6 @@
                 </a>
                 @endif
 
-
-                @if(auth()->user()->role === 'petugas')
-                <a href="{{ route('petugas.dashboard') }}" class="block px-4 py-2 rounded-lg transition {{ request()->routeIs('petugas.dashboard*') ? 'bg-gray-800 text-white font-medium shadow' : 'text-gray-400 hover:bg-gray-800 hover:text-white' }}">Dashboard</a>
-                <a href="{{ route('petugas.peminjaman.index') }}" class="block px-4 py-2 rounded-lg transition {{ request()->routeIs('petugas.peminjaman*') ? 'bg-gray-800 text-white font-medium shadow' : 'text-gray-400 hover:bg-gray-800 hover:text-white' }}">Persetujuan Peminjaman</a>
-                <a href="{{ route('petugas.pengembalian.index') }}" class="block px-4 py-2 rounded-lg transition {{ request()->routeIs('petugas.pengembalian*') ? 'bg-gray-800 text-white font-medium shadow' : 'text-gray-400 hover:bg-gray-800 hover:text-white' }}">Pemantauan Pengembalian</a>
-                <a href="{{ route('petugas.pesan.index') }}" class="block px-4 py-2 rounded-lg {{ request()->routeIs('petugas.pesan*') ? 'bg-gray-800 text-white' : 'text-gray-400 hover:bg-gray-800 hover:text-white' }}">Riwayat Laporan Saya</a>
-                <a href="{{ route('petugas.laporan.index') }}" class="block px-4 py-2 rounded-lg transition {{ request()->routeIs('petugas.laporan*') ? 'bg-gray-800 text-white font-medium shadow' : 'text-gray-400 hover:bg-gray-800 hover:text-white' }}">Cetak Laporan</a>
-                @endif
-
                 @if(auth()->user()->role === 'peminjam')
                 <a href="{{ route('peminjam.katalog') }}" class="block px-4 py-2 rounded-lg transition {{ request()->routeIs('peminjam.katalog*') ? 'bg-gray-800 text-white font-medium shadow' : 'text-gray-400 hover:bg-gray-800 hover:text-white' }}">
                     Katalog Alat
